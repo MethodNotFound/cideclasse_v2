f5a047aa21f65c6241d587f1ed7a81da9630afd0
breno <breno.nunes@segarante.com.br>
2024-10-23 23:33:45 -0300

    more ui

--- app/controllers/task_student_controller.rb
+++ app/controllers/task_student_controller.rb
@@ -3,5 +3,16 @@ class TaskStudentController < ApplicationController
     @sidebar = true
     @task = Task.find(params[:task_id])
     @student = current_student.student
+
+    ##################33
+
+    @sidebar = true
+    @submission = Submission.new
+    @task = Task.find(params[:task_id])
+    @submission.task = @task
+
+    @previous_submissions = @task.submissions.where(student: current_student.student)
+
+    @inspected = Submission.find(params[:inspect]) if params[:inspect]
   end
 end
