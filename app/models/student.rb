0ced999587218b18cc1afbe385e3d0de067ffb7f
breno <breno.nunes@segarante.com.br>
2024-10-24 10:26:09 -0300

    me mata

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,6 @@ class Student < ApplicationRecord
   # attr_accessor :password_digest
   has_secure_password
   has_many :sessions
+  has_many :submissions
   has_and_belongs_to_many :klasses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_11_114934) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_24_125841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_11_114934) do
     t.bigint "student_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "passed"
     t.index ["student_id"], name: "index_submissions_on_student_id"
     t.index ["task_id"], name: "index_submissions_on_task_id"
   end
